09cd372002a3959d91952bac7c282376d7368d1f
David Fernandez <dev@10io.net>
2013-05-05 12:18:44 +0200

    Add stopwatch support

--- TimeKeeper/AppDelegate.rb
+++ TimeKeeper/AppDelegate.rb
@@ -5,37 +5,69 @@
 #  Created by David Fernandez on 04/05/2013.
 #  Copyright 2013 David Fernandez. All rights reserved.
 #
-
 class AppDelegate
+    #outlets
     attr_accessor :window
-    attr_accessor :text_field #outlet
+    attr_accessor :date_field
+    attr_accessor :time_field
+    attr_accessor :remaining_time_field 
+    
+    attr_accessor :start_button
+    attr_accessor :pause_button
+    attr_accessor :unpause_button
     
     def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+      @stopwatch = Stopwatch.new
+      repaint(nil)
+      NSTimer.scheduledTimerWithTimeInterval(0.5,
+      target: self,
+      selector: "repaint:",
+      userInfo: nil,
+      repeats: true)
     end
     
-    def startTimer(sender)
-      if @timer.nil?
-            @time = 0.0
-            @timer = NSTimer
-                      .scheduledTimerWithTimeInterval(0.1,
-                                                      target: self,
-                                                      selector: "timerHandler:",
-                                                      userInfo: nil,
-                                                      repeats: true)
-      end
+    def start(sender)
+      @stopwatch.start
     end
-
-    def stopTimer(sender)
-      if @timer
-        @timer.invalidate
-        @timer = nil
+    
+    def pause(sender)
+      @stopwatch.pause
+    end
+    
+    def unpause(sender)
+      @stopwatch.unpause
+    end
+    
+    def repaint(userInfo)
+      time = Time.now
+      date_field.setStringValue(time.strftime(" %b %d, %Y "))
+      time_field.setStringValue(time.strftime("%I:%M.%S"))
+      remaining_text = @stopwatch.remaining
+      remaining_text += " (Paused)" if @stopwatch.paused?
+      remaining_time_field.setStringValue(remaining_text)
+      
+      hide(start_button)
+      hide(pause_button)
+      hide(unpause_button)
+      
+      if !@stopwatch.counting?
+        unhide(start_button)
+      end
+      if @stopwatch.running?
+        unhide(pause_button)
+      end
+      if @stopwatch.paused?
+        unhide(unpause_button)
       end
     end
     
-    def timerHandler(userInfo)
-      @time += 0.1
-      string = sprintf("%.1f", @time)
-      text_field.setStringValue(string)
+    def hide(object)
+      object.setTransparent(true)
+      object.setEnabled(false)
+    end
+    
+    def unhide(object)
+      object.setTransparent(false)
+      object.setEnabled(true)
     end
 end
